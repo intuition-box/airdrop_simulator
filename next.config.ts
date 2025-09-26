aca446f2a84d42faddc4124b1fdf437ab37a1360
Jade-m22 <mj.micheljade@gmail.com>
2025-09-27 01:42:06 +0200

    fix mainpage ui

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ export default {
   output: "export",
   images: { unoptimized: true },
   basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}` : "",
   trailingSlash: true,
   env: {
     NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : "",
